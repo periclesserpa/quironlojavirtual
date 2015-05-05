80e0711e7260ab98ff68f08400a9aa86ee17ec46
periclesserpa <peke1_750@hotmail.com>
2015-05-04 21:00:33 -0300

    Pedido

---
+++ Quiron.LojaVirtual.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
+{
+    public class Pedido
+    {
+        [Required(ErrorMessage="Informe seu nome")]
+
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "Cep:")]
+        public string CEP { get; set; }
+
+        [Required(ErrorMessage = "Informe seu endereço")]
+        [Display(Name = "Endereço:")]
+        public string Endedeco { get; set; }
+
+        [Display(Name = "Complemento:")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Informe a cidade")]
+        [Display(Name = "Cidade:")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe seu bairro")]
+        [Display(Name = "Bairro:")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Informe seu e-mail")]
+        [Display(Name = "E-mail:")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "Embrulhar para presente?")]        
+        public bool EmbrulhaPresente { get; set; }
+    }
+}
